e37cb1b59b76f6ca81761f5d547f0fc9ac442b9b
beesou777 <sbeesou@gmail.com>
2022-11-06 15:00:13 +0545

    book api hope you like my project

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,18 +1,21 @@
 
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import './styles/banner.css'
 import axios from 'axios'
 import Book from './Book'
 const Banner = () => {
     const[search,setsearch] = useState("")
     const[data,setdata] = useState([])
-    const searchBook =(event)=>{
-        if(event.key==="Enter"){
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+ search+'&key=AIzaSyBNYJPSdxq9HDUkhWJtFxjFqPi7OaHaInA'+'&maxResults=40').
-            then(res=>setdata(res.data.items))
-            .catch(err=>console.log(err))
-        }
-    }
+    const[button,setButton] = useState('')
+
+    useEffect(()=>{
+                axios.get('https://www.googleapis.com/books/v1/volumes?q='+ button+'&key=AIzaSyBNYJPSdxq9HDUkhWJtFxjFqPi7OaHaInA'+'&maxResults=40').
+                then(res=>setdata(res.data.items))
+                .catch(err=>console.log(err))
+    },[button])
+    const handleClick = (()=>{
+        setButton(search)
+    })
     return (
         <>
 
@@ -21,8 +24,8 @@ const Banner = () => {
             <div className="sea">
                 <input type="text" placeholder='search here' id='a' 
                 value={search} onChange={e=>setsearch(e.target.value)} 
-                onKeyDown={searchBook}/>
-                <button className='btn2'onClick={()=>setsearch(searchBook)}><i class="fas fa-search"></i></button>
+                />
+                <button className='btn2' onClick={handleClick}><i class="fas fa-search"></i></button>
             </div>
         </div>
         <div className="book">
